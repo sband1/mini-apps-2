0c2aef9a0197430236bfd1538e62026696b1c7ba
sband1 <sbandeali1@gmail.com>
2021-01-07 15:04:35 -0800

    Challenge2 webpack setup

--- challenge_2/client/app.jsx
+++ challenge_2/client/app.jsx
@@ -1,2 +1,19 @@
+import React from 'react';
 
+class  extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default ;
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
 
+const port = 3000;
 
+const publicDir = path.join(__dirname, './public');
+app.use(bodyParser.json());
+app.use(express.static(publicDir));
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
