d5c99ccc2bbae968bdff5e75f76ba33022ae8b14
sband1 <sbandeali1@gmail.com>
2021-01-07 20:27:37 -0800

    Implement api call to coinDesk

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
+const axios = require('axios');
 
 const port = 3000;
 
@@ -9,10 +10,22 @@ const publicDir = path.join(__dirname, './public');
 app.use(bodyParser.json());
 app.use(express.static(publicDir));
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.get('/historicalPrices', (req, res) => {
+  console.log('req.body', req.query);
+  axios({
+    method: 'get',
+    url: `https://api.coindesk.com/v1/bpi/historical/close.json?start=${req.query.startDate}&end=${req.query.endDate}`,
+  })
+    .then((result) => {
+      console.log('get historical prices success');
+      res.status(202).json(result.data.bpi);
+    })
+    .catch((err) => {
+      console.log('get historical prices error', err);
+      res.sendStatus(404);
+    });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
