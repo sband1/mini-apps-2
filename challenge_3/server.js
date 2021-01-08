9b2e9db6b09017d7f23f31a82c328e709741ad57
sband1 <sbandeali1@gmail.com>
2021-01-08 12:02:47 -0800

    Express server serving up index.html

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
 
+const publicDir = path.join(__dirname, './public');
+app.use(express.static(publicDir));
 
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
