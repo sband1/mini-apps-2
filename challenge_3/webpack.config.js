220e538fba5f424fd800796948439f279ffe1607
sband1 <sbandeali1@gmail.com>
2021-01-08 12:01:26 -0800

    Webpack and index.html set-up

---
+++ challenge_3/webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var DIST_DIR = path.join(__dirname, '/public');
+var SRC_DIR = path.join(__dirname, '/client');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.m?js|jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  mode: 'development',
+};
